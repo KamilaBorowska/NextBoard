77442bcdfbc4d43071eff705021ef089946be0f5
Konrad Borowski <x.fix@o2.pl>
2014-07-13 18:45:12 +0200

    Test author() method.

--- forum/tests.py
+++ forum/tests.py
@@ -5,11 +5,13 @@ class PostTestCase(TestCase):
     def setUp(self):
         """Prepare for testing posts."""
         user = User.objects.create(username='SampleGuy')
+        author = User.objects.create(username='RelevantAuthor')
 
         thread = Thread.objects.create(title='This is it')
         post = Post.objects.create(thread=thread)
 
-        for content in ['A', 'B', 'C', 'D', 'E']:
+        PostRevision.objects.create(post=post, text='A', author=author)
+        for content in ['B', 'C', 'D', 'E']:
             PostRevision.objects.create(post=post, text=content, author=user)
 
         # Create unrelated thread to find possible issues in query.
@@ -18,6 +20,7 @@ class PostTestCase(TestCase):
         PostRevision.objects.create(post=unrelated_post, text='F', author=user)
 
         self.post = post
+        self.author = author
 
     def test_first_revision(self):
         """First revision should be the initial revision."""
@@ -26,3 +29,6 @@ class PostTestCase(TestCase):
     def test_last_revision(self):
         """Last revision should be the final revision."""
         self.assertEqual(self.post.last_revision().text, 'E')
+
+    def test_author(self):
+        self.assertEqual(self.post.author(), self.author)
