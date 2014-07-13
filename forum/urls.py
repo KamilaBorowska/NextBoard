8290a418b8387b3e073c96cba6f8e8af2334a4c6
Konrad Borowski <x.fix@o2.pl>
2014-07-13 20:07:37 +0200

    Create view for threads.

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from forum import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.ForumView.as_view(), name='forums')
+    url(r'^$', views.ForumView.as_view(), name='forums'),
+    url(r'^forums/(?P<forum_id>\d+)/$', views.ThreadView.as_view(), name='forum'),
 )
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,9 @@
 from django.views import generic
 
-from forum.models import Forum
+from forum.models import Forum, Thread
 
 class ForumView(generic.ListView):
     model = Forum
+
+class ThreadView(generic.ListView):
+    model = Thread
