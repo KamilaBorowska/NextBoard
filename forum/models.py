374924eae80072f784059c8741daef1a438f1dbf
Konrad Borowski <x.fix@o2.pl>
2014-07-13 15:41:14 +0200

    Forums have threads.

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Thread(models.Model):
+    """Model for representing threads."""
+    title = models.TextField()
+    views = models.PositiveIntegerField(default=0)
+    sticky = models.BooleanField()
+    closed = models.BooleanField()
