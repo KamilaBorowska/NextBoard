a6dc99096870b186aafe85e9777b7c29de488a51
Konrad Borowski <x.fix@o2.pl>
2014-07-13 15:53:13 +0200

    Create user class for forums.
    Forums need to store information users that is usually not stored.
    This currently stores only display name, althrough it can store more
    in the future.

--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+import django.contrib.auth.models as auth
+
+class User(auth.User):
+    """Model for representing users.
+
+    It has few fields that aren't in the standard authentication user
+    table, and are needed for the forum to work, like footers.
+    """
+    display_name = models.CharField(max_length=30, null=True)
+
 
 class Thread(models.Model):
     """Model for representing threads."""
